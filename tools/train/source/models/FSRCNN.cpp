a32836a680c645e446b3c3c22cca672eeaa36bd2
Zhenqing Hu <hustc12@gmail.com>
2024-06-20 18:00:24 -0400

    Updated the FSRCNN model by implementing the Mapping Blocks.
    Signed-off-by: Zhenqing Hu <hustc12@gmail.com>

--- tools/train/source/models/FSRCNN.cpp
+++ tools/train/source/models/FSRCNN.cpp
@@ -78,7 +78,41 @@ std::shared_ptr<Module> Shrinking(std::vector<int> inputOutputChannels, int kern
 }
 
 ///// Mapping
+class _MappingBlock: public Module {
+public:
+    _MappingBlock(std::vector<int> inputOutputChannels, int kernelSize, int stride, int padding,
+             bool depthwise);
+    virtual std::vector<Express::VARP> onForward(const std::vector<Express::VARP> &inputs) override;
 
+    std::shared_ptr<Module> conv2d_mapping;
+};
+
+_MappingBlock::_MappingBlock(std::vector<int> inputOutputChannels, int kernelSize, int stride, int padding,
+       bool depthwise) {
+    int inChannels = inputOutputChannels[0], outChannels = inputOutputChannels[1];
+    NN::ConvOption convOption;
+    convOption.kernelSize = {kernelSize, kernelSize};
+    convOption.channel = {inChannels, outChannels};
+    convOption.stride = {stride, stride};
+    convOption.pads = {padding, padding};
+    convOption.depthwise = depthwise;
+
+    conv2d_mapping.reset(NN::Conv(convOption, true, std::shared_ptr<Initializer>(Initializer::MSRA())));
+    registerModel({conv2d_mapping});
+}
+
+std::vector<Express::VARP> _MappingBlock::onForward(const std::vector<Express::VARP> &inputs) {
+    using namespace Express;
+    VARP x = inputs[0];
+    x = conv2d_mapping->forward(x);
+    // TODO: PReLU
+    return {x};
+}
+
+std::shared_ptr<Module> MappingBlock(std::vector<int> inputOutputChannels, int kernelSize, int stride, int padding,
+                                     bool depthwise) {
+    return std::shared_ptr<Module>(new _MappingBlock(inputOutputChannels, kernelSize, stride, padding, depthwise));
+}
 
 //// Expanding
 class _Expanding: public Module {
@@ -155,16 +189,14 @@ std::shared_ptr<Module> Deconvolution(std::vector<int> inputOutputChannels, int 
 FSRCNN::FSRCNN(int num_channels, int d, int s, int m, int upscale_factor) {
 
     // 1. Feature Extraction
-    // TODO: Double check the encoder_layer
     feature_extraction = FeatureExtraction({1, 56}, 5, 1, 2);
 
     // 2. Shrinking
     shrinking = Shrinking({56, 12}, 1, 1, 0);
 
     // 3. Mapping
-    // TODO: Double check the encoder_layer
     for (int i=0; i<m; i++) {
-        encoder_layers.emplace_back(EncoderBlock());
+        mapping.emplace_back(MappingBlock({12,12}, 3,1,1, false));
     }
 
     // 4. Expanding
@@ -174,7 +206,7 @@ FSRCNN::FSRCNN(int num_channels, int d, int s, int m, int upscale_factor) {
     deconvolution = Deconvolution({56, 1}, 9, 3, 4, 2, false);
 
     registerModel({feature_extraction, shrinking, expanding, deconvolution});
-    registerModel(encoder_layers);
+    registerModel(mapping);
 }
 
 std::vector<Express::VARP> FSRCNN::onForward(const std::vector<Express::VARP> &inputs) {
